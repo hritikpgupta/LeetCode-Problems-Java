f0c77b3c805f686a1dc7e616ac4e4d4a3982efb1
hritikpgupta <hritikgupta722@gmail.com>
2023-04-26 17:07:05 +0100

    RomainToInteger

---
+++ src/main/java/org/example/romanToInteger/RomanToInteger.java
@@ -0,0 +1,28 @@
+package org.example.romanToInteger;
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        System.out.println("hgh");
+        String s = "XIII";
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int res = map.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                res -= map.get(s.charAt(i));
+            }else{
+                res += map.get(s.charAt(i));
+            }
+        }
+        System.out.println(res);
+    }
+}
