bdbd2002e1d14ca80ee988235d8c06a041bfbd5c
hritikpgupta <hritikgupta722@gmail.com>
2023-04-26 16:31:20 +0100

    Palindrome

--- src/main/java/org/example/addLinkedList/ListNode.java
+++ src/main/java/org/example/addLinkedList/ListNode.java
@@ -1,17 +1,21 @@
-package org.example;
+package org.example.addLinkedList;
 
 public class ListNode {
     int val;
     ListNode next;
+
     ListNode() {
     }
-    ListNode(int val) {
+
+    public ListNode(int val) {
         this.val = val;
     }
+
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
+
     @Override
     public String toString() {
         return "ListNode{" +
---
+++ src/main/java/org/example/palindrome/Main.java
@@ -0,0 +1,16 @@
+package org.example.palindrome;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("1234543210"));
+    }
+
+    static boolean isPalindrome(String value) {
+        int len = value.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (value.charAt(i) != value.charAt(len - i - 1)) return false;
+        }
+        return true;
+    }
+}
